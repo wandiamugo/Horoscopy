1236cc37b66ec9080881ae592b4d5911a5e218a0
Wandia E Mugo <wwandia173@gmail.com>
2022-07-26 10:52:24 +0300

    App.js code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,21 @@ import Container from './components/Container/Container';
 import Header from './components/Header/Header';
 import './App.css';
 
-// class App extends Component {
-//   constructor (props) {
-//     super(props)
-//   }
-// }
+ class App extends Component {
+    constructor (props) {
+    super(props)
+    this.state = {
+      currentSign: 'sagittarius'
+    }
+   }
+   render() {
+    return (
+      <div className="App">
+        <Header />
+        <Container currentSign= {this.state.currentSign}/>
+      </div>
+    );
+   }
+}
+
+export default App;
