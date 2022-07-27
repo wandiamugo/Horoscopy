38697b23aaffdc1bc7e6604f29a4ce20fbb943c8
wandiamugo <wwandia173@gmail.com>
2022-07-27 18:49:11 +0300

    changes

--- Wandia/README.md
+++ Wandia/README.md
@@ -5,3 +5,14 @@ It uses React to render the page and Aztro API to fetch the horoscopes.
 #### How to use it
 ##### Just Clone and `npm start` it or  - [click here to see a demo](https://sergekashkin.github.io/daily_scope/).
 ![Image](https://user-images.githubusercontent.com/39168159/114576513-e285cb00-9c83-11eb-8f96-03388a61cbe6.gif)
+
+
+create a new branch and use the following commands
+
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bensonmaina/wandia.git   //replace wandia with the repo name
+git push -u origin main
--- my-app/src/components/Change/Change.js
+++ my-app/src/components/Change/Change.js
@@ -1,3 +1,50 @@
+// import './Change.css'
+// import React from 'react';
+
+
+// const Change = ({changeSign}) => {
+
+
+//     const signName = (e) => {
+//         let signon = e.target.src.split('/')[6].split('.')[0];
+//         document.getElementById('cur_sign').textContent = signon.charAt(0).toUpperCase() + signon.slice(1);
+//     }
+
+//     const clear = () => {
+//         document.getElementById('cur_sign').textContent=""
+//     }
+
+//     const change = (e) => {
+//         let signon = e.target.src.split('/')[6].split('.')[0];
+//         changeSign(signon);
+//     }
+//     return (
+//         <div className="card2">
+//             <div className="title"><p>Choose Your Sign</p></div>
+//             <div className="flex" id="first">
+//                 <img className="sign" src={require('../../icons/aquarius 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/aries 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/cancer 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/capricorn 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/gemini 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/leo 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//             </div>
+//             <div id="cur_sign"></div>
+//             <div className="flex" id="second">
+//                 <img className="sign" src={require('../../icons/libra 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/pisces 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/sagittarius 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/scorpio 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/taurus 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//                 <img className="sign" src={require('../../icons/virgo 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+//             </div>
+//             <div></div>
+//         </div>
+//         )
+//     }
+    
+    
+//     export default Change;
 import './Change.css'
 import React from 'react';
 
@@ -18,30 +65,31 @@ const Change = ({changeSign}) => {
         let signon = e.target.src.split('/')[6].split('.')[0];
         changeSign(signon);
     }
+
     return (
-        <div className="card2">
-            <div className="title"><p>Choose Your Sign</p></div>
-            <div className="flex" id="first">
-                <img className="sign" src={require('../../icons/aquarius 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/aries 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/cancer 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/capricorn 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/gemini 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/leo 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-            </div>
-            <div id="cur_sign"></div>
-            <div className="flex" id="second">
-                <img className="sign" src={require('../../icons/libra 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/pisces 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/sagittarius 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/scorpio 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/taurus 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-                <img className="sign" src={require('../../icons/virgo 2.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
-            </div>
-            <div></div>
+    <div className="card2">
+        <div className="title"><p>Choose Your Sign</p></div>
+        <div className="flex" id="first">
+            <img className="sign" src={require('../../signs/aquarius.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/aries.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/cancer.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/capricorn.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/gemini.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/leo.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
         </div>
-        )
-    }
-    
-    
-    export default Change;
+        <div id="cur_sign"></div>
+        <div className="flex" id="second">
+            <img className="sign" src={require('../../signs/libra.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/pisces.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/sagittarius.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/scorpio.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/taurus.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+            <img className="sign" src={require('../../signs/virgo.png')} onMouseEnter={(e) => signName(e)} onMouseOut={() => clear()} onClick={(e) => change(e)}/>
+        </div>
+        <div></div>
+    </div>
+    )
+}
+
+
+export default Change;
